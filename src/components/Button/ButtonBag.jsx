960183e1213b20fc9739ea821e3ddcae32d4d442
Nikolay Geldyev <nikoejento@gmail.com>
2020-12-20 13:13:05 +0300

    add Icon bag and bag button components

---
+++ src/components/Button/ButtonBag.jsx
@@ -0,0 +1,65 @@
+import React, { useContext, useState } from 'react';
+import styled from 'styled-components';
+import PropTypes, { bool } from 'prop-types';
+
+import Button from './Button';
+import IconBag from '../Icons/IconBag';
+import { ThemeContext } from '../../theme-context';
+
+const StyledButton = styled(Button)`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  font-size: 0;
+  line-height: 0;
+  background-color: ${(props) => props.bgColor};
+  ${({ isActive }) => (isActive ? 'box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.08)' : '')};
+`;
+function ButtonBag({ handleClick, isActive }) {
+  const [pressed, setPressed] = useState(false);
+  const theme = useContext(ThemeContext);
+  const handlePress = () => {
+    setPressed(true);
+    const handleMouseUp = () => {
+      document.removeEventListener('mouseup', handleMouseUp);
+      setPressed(false);
+    };
+    document.addEventListener('mouseup', handleMouseUp);
+  };
+  let bgColor;
+  let fill;
+  if (pressed) {
+    fill = theme.light;
+    bgColor = theme.primary;
+  } else if (isActive) {
+    fill = theme.primary;
+    bgColor = theme.main;
+  } else {
+    fill = theme.light;
+    bgColor = theme.primary;
+  }
+  return (
+    <StyledButton
+      size="small"
+      handleClick={handleClick}
+      theme={theme}
+      bgColor={bgColor}
+      isActive={isActive}
+      onMouseDown={handlePress}
+    >
+      add to bag
+      <IconBag fill={fill} />
+    </StyledButton>
+  );
+}
+
+ButtonBag.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  isActive: bool,
+};
+
+ButtonBag.defaultProps = {
+  isActive: false,
+};
+
+export default ButtonBag;
