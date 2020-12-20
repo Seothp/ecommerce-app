76f26e10b1dff343c2d6da8b9a8d856c5ce87eec
Nikolay Geldyev <nikoejento@gmail.com>
2020-12-20 13:34:38 +0300

    create add button and plus icon components

---
+++ src/components/Button/ButtonAdd.jsx
@@ -0,0 +1,48 @@
+import React, { useContext, useState } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import { ThemeContext } from '../../theme-context';
+import IconPlus from '../Icons/IconPlus';
+
+const StyledButton = styled.button`
+  border: none;
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  font-size: 0;
+  line-height: 0;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.08);
+  background-color: ${(props) => (props.pressed ? props.theme.main : props.theme.invert)};
+`;
+
+function ButtonAdd({ handleClick }) {
+  const [pressed, setPressed] = useState(false);
+  const theme = useContext(ThemeContext);
+  const handlePress = () => {
+    setPressed(true);
+    const handleMouseUp = () => {
+      document.removeEventListener('mouseup', handleMouseUp);
+      setPressed(false);
+    };
+    document.addEventListener('mouseup', handleMouseUp);
+  };
+  const fill = pressed ? theme.invert : theme.main;
+  return (
+    <StyledButton
+      handleClick={handleClick}
+      onMouseDown={handlePress}
+      theme={theme}
+      pressed={pressed}
+    >
+      Add
+      <IconPlus fill={fill} />
+    </StyledButton>
+  );
+}
+
+ButtonAdd.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
+
+export default ButtonAdd;
