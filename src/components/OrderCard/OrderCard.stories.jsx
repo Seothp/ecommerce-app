c02fa91b2c50c5d944ce259ce62f0c045315225d
Nikolay <nikoejento@gmail.com>
2021-02-05 14:56:10 +0300

    add OrderCard story

---
+++ src/components/OrderCard/OrderCard.stories.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import OrderCard from './OrderCard';
+
+export default {
+  title: 'Components/OrderCard',
+  component: OrderCard,
+};
+
+const Template = (args) => <OrderCard {...args} />;
+
+export const Delivered = Template.bind({});
+Delivered.args = {
+  id: 848923,
+  trackId: '94D83AB21331',
+  date: '05-12-2008',
+  quantity: 3,
+  amount: 150,
+  onOpenDetails: () => {},
+  deliverStatus: 'delivered',
+};
+
+export const Cancelled = Template.bind({});
+Cancelled.args = {
+  ...Delivered.args,
+  deliverStatus: 'cancelled',
+};
