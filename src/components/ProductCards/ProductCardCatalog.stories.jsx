802b1b9f4c36809e6f1b6001ab956be45993c96c
Nikolay <nikoejento@gmail.com>
2021-02-05 16:01:26 +0300

    add ProductCardCatalog story

---
+++ src/components/ProductCards/ProductCardCatalog.stories.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import ProductCardCatalog from './ProductCardCatalog';
+
+export default {
+  title: 'Components/ProductCards/ProductCardCataglog',
+  component: ProductCardCatalog,
+};
+
+const Template = (args) => <ProductCardCatalog {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  imgSrc: 'https://via.placeholder.com/100',
+  price: 150,
+  item: 't-shirt',
+  brand: 'gucci',
+  rating: 8.5,
+  countOfVotes: 23,
+  onAddToFavorites: () => {},
+};
+export const Line = Template.bind({});
+Line.args = {
+  ...Default.args,
+  type: 'line',
+};
+export const LineWithNewPrice = Template.bind({});
+LineWithNewPrice.args = {
+  ...Default.args,
+  type: 'line',
+  newPrice: 80,
+};
+export const Module = Template.bind({});
+Module.args = {
+  ...Default.args,
+  type: 'module',
+};
+export const ModuleWithNewPrice = Template.bind({});
+ModuleWithNewPrice.args = {
+  ...Default.args,
+  type: 'module',
+  newPrice: 80,
+};
+export const Main = Template.bind({});
+Main.args = {
+  ...Default.args,
+  type: 'module',
+  main: true,
+  newPrice: 80,
+};
+export const MainWithNewPrice = Template.bind({});
+MainWithNewPrice.args = {
+  ...Default.args,
+  type: 'module',
+  main: true,
+  newPrice: 80,
+};
