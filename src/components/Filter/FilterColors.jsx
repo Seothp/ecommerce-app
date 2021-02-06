7605b6aa11d2e5f4e4312e3340908080e4a1a5d6
Nikolay <nikoejento@gmail.com>
2021-02-06 16:30:42 +0300

    correct param name

--- src/components/Filter/FilterColors.jsx
+++ src/components/Filter/FilterColors.jsx
@@ -6,7 +6,7 @@ import FilterColor from './FilterColor';
 /**
  *
  * @param {object} props - props of component
- * @param {Array.<{colorValue: String, isActive: boolean}>} props.color
+ * @param {Array.<{colorValue: String, isActive: boolean}>} props.colors
  */
 
 function FilterColors({ colors, onChange }) {
