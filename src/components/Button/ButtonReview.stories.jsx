ce128150b169d36a6a746baaf3e1bb9a7eb093cf
Nikolay <nikoejento@gmail.com>
2021-02-09 15:52:06 +0300

    add actions to story

--- src/components/Button/ButtonReview.stories.jsx
+++ src/components/Button/ButtonReview.stories.jsx
@@ -5,11 +5,12 @@ import ButtonReview from './ButtonReview';
 export default {
   title: 'Components/ButtonReview',
   component: ButtonReview,
+  argTypes: {
+    handleClick: { action: 'click' },
+  },
 };
 
 const Template = (args) => <ButtonReview {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {
-  handleClick: () => {},
-};
+Default.args = {};
