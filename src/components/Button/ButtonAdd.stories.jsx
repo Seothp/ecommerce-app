bfc490b1a2f99907c6f8f604507d852565a00cd8
Nikolay <nikoejento@gmail.com>
2021-02-09 15:47:12 +0300

    add actions to story

--- src/components/Button/ButtonAdd.stories.jsx
+++ src/components/Button/ButtonAdd.stories.jsx
@@ -5,11 +5,12 @@ import ButtonAdd from './ButtonAdd';
 export default {
   title: 'Components/ButtonAdd',
   component: ButtonAdd,
+  argTypes: {
+    handleClick: { action: 'clicked' },
+  },
 };
 
 const Template = (args) => <ButtonAdd {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {
-  handleClick: () => {},
-};
+Default.args = {};
