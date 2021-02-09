b87f4eacbbec38c82408d2425e2ff77aca0a9ca6
Nikolay <nikoejento@gmail.com>
2021-02-09 15:49:24 +0300

    add actions to story

--- src/components/Button/ButtonClose.stories.jsx
+++ src/components/Button/ButtonClose.stories.jsx
@@ -5,11 +5,12 @@ import ButtonClose from './ButtonClose';
 export default {
   title: 'Components/ButtonClose',
   component: ButtonClose,
+  argTypes: {
+    onClick: { action: 'click' },
+  },
 };
 
 const Template = (args) => <ButtonClose {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {
-  onClick: () => {},
-};
+Default.args = {};
