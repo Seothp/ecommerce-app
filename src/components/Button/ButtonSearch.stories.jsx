94c022c8b923f6b1022e858afa3a8b24cc9ba725
Nikolay <nikoejento@gmail.com>
2021-02-09 15:52:29 +0300

    add actions to story

--- src/components/Button/ButtonSearch.stories.jsx
+++ src/components/Button/ButtonSearch.stories.jsx
@@ -5,11 +5,12 @@ import ButtonSearch from './ButtonSearch';
 export default {
   title: 'Components/ButtonSearch',
   component: ButtonSearch,
+  argTypes: {
+    handleClick: { action: 'click' },
+  },
 };
 
 const Template = (args) => <ButtonSearch {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {
-  handleClick: () => {},
-};
+Default.args = {};
