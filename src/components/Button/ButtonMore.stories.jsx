c7d3754ad37b4d0d8a52cedc3f44708dc8be7713
Nikolay <nikoejento@gmail.com>
2021-02-09 15:51:41 +0300

    add actions to story

--- src/components/Button/ButtonMore.stories.jsx
+++ src/components/Button/ButtonMore.stories.jsx
@@ -5,11 +5,12 @@ import ButtonMore from './ButtonMore';
 export default {
   title: 'Components/ButtonMore',
   component: ButtonMore,
+  argTypes: {
+    onClick: { action: 'click' },
+  },
 };
 
 const Template = (args) => <ButtonMore {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {
-  onClick: () => {},
-};
+Default.args = {};
