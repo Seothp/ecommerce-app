f858842dc89060a349905dd24a87a22c66c25d69
Nikolay <nikoejento@gmail.com>
2021-02-09 16:00:40 +0300

    add actions to story

--- src/components/Button/ButtonSocial.stories.jsx
+++ src/components/Button/ButtonSocial.stories.jsx
@@ -5,18 +5,19 @@ import ButtonSocial from './ButtonSocial';
 export default {
   title: 'Components/ButtonSocial',
   component: ButtonSocial,
+  argTypes: {
+    handleClick: { action: 'click' },
+  },
 };
 
 const Template = (args) => <ButtonSocial {...args} />;
 
 export const Facebook = Template.bind({});
 Facebook.args = {
-  handleClick: () => {},
   type: 'fb',
 };
 
 export const Google = Template.bind({});
 Google.args = {
-  handleClick: () => {},
   type: 'google',
 };
